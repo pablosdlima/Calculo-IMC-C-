2452b81c2f0716dc9d3d5bcdbb10e332846e424c
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2020-04-30 21:19:02 -0300

    Projeto IMC em C# windows form com imagem

---
+++ Calculo IMC/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Calculo_IMC
+{
+    partial class lblPeso
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblImc = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnLimp = new System.Windows.Forms.Button();
+            this.btnCanc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(63, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.ForeColor = System.Drawing.Color.White;
+            this.lblAltura.Location = new System.Drawing.Point(63, 122);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(59, 23);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblImc
+            // 
+            this.lblImc.AutoSize = true;
+            this.lblImc.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImc.ForeColor = System.Drawing.Color.White;
+            this.lblImc.Location = new System.Drawing.Point(63, 185);
+            this.lblImc.Name = "lblImc";
+            this.lblImc.Size = new System.Drawing.Size(44, 23);
+            this.lblImc.TabIndex = 2;
+            this.lblImc.Text = "IMC";
+            this.lblImc.Click += new System.EventHandler(this.lblImc_Click);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(154, 57);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(117, 20);
+            this.txtPeso.TabIndex = 3;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(154, 126);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(117, 20);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(504, 47);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(95, 42);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnLimp
+            // 
+            this.btnLimp.Location = new System.Drawing.Point(504, 126);
+            this.btnLimp.Name = "btnLimp";
+            this.btnLimp.Size = new System.Drawing.Size(95, 42);
+            this.btnLimp.TabIndex = 6;
+            this.btnLimp.Text = "Limpar";
+            this.btnLimp.UseVisualStyleBackColor = true;
+            this.btnLimp.Click += new System.EventHandler(this.btnLimp_Click);
+            // 
+            // btnCanc
+            // 
+            this.btnCanc.Location = new System.Drawing.Point(504, 206);
+            this.btnCanc.Name = "btnCanc";
+            this.btnCanc.Size = new System.Drawing.Size(95, 42);
+            this.btnCanc.TabIndex = 7;
+            this.btnCanc.Text = "Cancelar";
+            this.btnCanc.UseVisualStyleBackColor = true;
+            // 
+            // lblPeso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCanc);
+            this.Controls.Add(this.btnLimp);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblImc);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.label1);
+            this.Name = "lblPeso";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.lblPeso_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblImc;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnLimp;
+        private System.Windows.Forms.Button btnCanc;
+    }
+}
+
---
+++ Calculo IMC/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_IMC
+{
+    public partial class lblPeso : Form
+    {
+        double peso, altura, imc;
+
+        private void btnLimp_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtPeso.Clear();
+            lblImc.Text = "";
+
+
+
+
+        }
+
+        private void lblPeso_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblImc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public lblPeso()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            peso = double.Parse(txtPeso.Text);
+            altura = double.Parse(txtAltura.Text);
+
+            imc = peso / altura;
+            lblImc.Text = "O IMC calculado é " + imc;
+        }
+    }
+}
